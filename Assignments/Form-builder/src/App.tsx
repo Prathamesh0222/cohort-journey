2e4a388d0120d09612eac4c7918c48edb41c8cc9
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-09-17 21:06:51 +0530

    Add initial project files for Form Builder assignment

---
+++ Assignments/Form-builder/src/App.tsx
@@ -0,0 +1,13 @@
+import FormBuilder from "./components/FormBuilder";
+
+function App() {
+  return (
+    <>
+      <div>
+        <FormBuilder />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Assignments/Form-builder/src/components/FormBuilder.tsx
@@ -0,0 +1,5 @@
+const FormBuilder = () => {
+  return <div className="container mx-auto p-4">Hi There</div>;
+};
+
+export default FormBuilder;
