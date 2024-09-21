da314e4d1c8e0265f1cf4b7241f45155d7632952
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-09-21 23:54:54 +0530

    Refactor Course and User models

--- Assignments/course-selling-app/backend/src/model/Course.ts
+++ Assignments/course-selling-app/backend/src/model/Course.ts
@@ -1,5 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 
 const courseSchema = new Schema({
-
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    imageUrl: { type: String, required: true },
+    creatorId: { type: Schema.Types.ObjectId, ref: "User", required: true }
 })
+
+const CourseModel = mongoose.model("Course", courseSchema);
+
+export default CourseModel;
--- Assignments/course-selling-app/backend/src/model/User.ts
+++ Assignments/course-selling-app/backend/src/model/User.ts
@@ -4,12 +4,15 @@ export interface User extends Document {
     email: string;
     password: string;
     username: string;
+    role: string;
 }
 
 const userSchema: Schema<User> = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    username: { type: String, required: true }
+    username: { type: String, required: true },
+    role: { type: String, required: true, enum: ["admin", "user"], default: "user" }
+
 })
 
 const UserModel = mongoose.model<User>("User", userSchema);
