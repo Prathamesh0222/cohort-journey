f31310fee613e38aa3dae8af7f0281cebace71e5
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-10-12 01:12:56 +0530

    Add function to find hash with specific prefix and starting string

--- web-3/Assignments/src/week-1/index.ts
+++ web-3/Assignments/src/week-1/index.ts
@@ -16,7 +16,22 @@ function findHashWithPrefix(prefix: string) {
   }
 }
 
+function findHashWithPrefix2(prefix: string, start: string) {
+  let i = 0;
+  while (true) {
+    const input = start + i;
+    const hash = getCryptoHash256(input);
+    if (hash.startsWith(prefix)) {
+      return { input, hash };
+    }
+    i++;
+  }
+}
+
 const prefix = "0000";
+const startString = "100xdevs";
 const result = findHashWithPrefix(prefix);
+const result2 = findHashWithPrefix2(prefix, startString);
 console.log(`Input is: ${result.input}`);
 console.log(`Output hash is: ${result.hash}`);
+console.log(`Input: ${result2.input}\nOutput: ${result2.hash}`);
