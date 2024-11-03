1e3e589906fae25aca2d1235e89921f580714ae6
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2024-11-03 15:45:59 +0530

    Implement Label, Input, and Button components

--- tailwind-class/src/App.tsx
+++ tailwind-class/src/App.tsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Label from "./components/Label";
+
 function App() {
-  return <>Hello There</>;
+  const [inputValue, setInputValue] = useState("");
+
+  const handleButton = () => {
+    console.log("Button clicked with input", inputValue);
+  };
+
+  return (
+    <div className="flex flex-col justify-center bg-blue-500 min-h-screen">
+      <div className="mx-auto">
+        <div>
+          <Label text={"Verify Your Age"} size={"large"} className="mb-8" />
+          <Label
+            text={
+              "Please confirm your birth year. This data will not be stored. "
+            }
+            size={"small"}
+            color={"gray"}
+            className="mb-4"
+          />
+          <Input
+            variant={"outlined"}
+            type={"text"}
+            placeholder={"Your Birth Date"}
+            fullWidth
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button
+            size={"medium"}
+            variant={"primary"}
+            onClick={handleButton}
+            label={"Continue"}
+            disabled={!inputValue.trim()}
+            className="mt-8"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
