2a581a68649ec6d5b2ecf43b42821f961aa600fc
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2024-11-03 15:45:02 +0530

    Enhance Tailwind CSS configuration with custom color palette

--- tailwind-class/tailwind.config.js
+++ tailwind-class/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          500: "#00285a",
+          700: "#153c69",
+        },
+        gray: {
+          500: "#7d91aa",
+          200: "#5b7492",
+        },
+      },
+    },
   },
   plugins: [],
 };
