ce079d44c8be1f212f8934944af2f5f00e860c95
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2024-11-04 23:51:07 +0530

    Add custom color extensions to Tailwind CSS configuration

--- Assignments/tailwind-assignment/tailwind.config.js
+++ Assignments/tailwind-assignment/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          500: "#e1e6eb",
+          200: "#f5f5f5",
+        },
+        blue: {
+          700: "#032758",
+        },
+      },
+    },
   },
   plugins: [],
 };
